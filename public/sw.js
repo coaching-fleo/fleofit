0a4fb31386f6c40d15ac978e95d53d73be3c9351
coaching-fleo <coaching@federicoleo.it>
2026-05-11 17:46:52 +0200

    push notifications

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: '/vite.svg', // Se hai un logo per la PWA, metti qui il percorso (es. /icon-192x192.png)
+      badge: '/vite.svg',
+      vibrate: [100, 50, 100],
+      data: {
+        url: data.url || '/'
+      }
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
