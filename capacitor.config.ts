16faef3b533d345680ea604adcbb304a657132e3
alepatrone <alessandro.patrone@hotmail.it>
2026-05-13 11:41:32 +0200

    First iOS commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fleofit.app',
+  appName: 'fleofit',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
